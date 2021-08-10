9c2fa30b3d60b9935174b760c9283c53bc4ddb00
Silviu Iordache <silviu.iordache92@gmail.com>
2021-08-10 10:03:16 +0300

    Hide header on scroll down

--- site/scripts/header.js
+++ site/scripts/header.js
@@ -1,23 +1,10 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  // console.log(window.scrollY)
-
-  if (window.scrollY > 150) {
-    document.getElementById("header").classList.remove('py-4');
-    document.getElementById("header").classList.add('py-3');
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.scrollY;
+  if ( prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = '0';
   } else {
-    document.getElementById("header").classList.remove('py-3');
-    document.getElementById("header").classList.add('py-4');
+    document.getElementById("header").style.top = '-100px';
   }
+  prevScrollpos = currentScrollPos;
 }
-
-var timer = null;
-window.addEventListener('scroll', function() {
-  if(timer !== null) {
-    clearTimeout(timer);        
-  }
-  timer = setTimeout(function() {
-    // console.log('user stopped scrolling')
-  }, 150);
-}, false);
