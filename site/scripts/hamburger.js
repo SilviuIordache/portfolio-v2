00120c62700fdbdfb8ff11759949602efea5dc65
Silviu Iordache <silviu.iordache92@gmail.com>
2021-09-01 22:19:50 +0300

    Implement hamburger

---
+++ site/scripts/hamburger.js
@@ -0,0 +1,19 @@
+export function hamburger() {
+  const trigger = document.getElementById('hamburger-trigger');
+  const menu = document.getElementById('hamburger-menu');
+  const content = document.getElementById('content');
+  const hamItems = document.getElementsByClassName('ham-item');
+  
+  trigger.addEventListener('click', () => {
+    menu.classList.toggle('show-hamburger');
+    content.classList.toggle('blur');
+  });
+
+
+  [...hamItems].forEach((hamItem) => {
+    hamItem.addEventListener('click', () => {
+      menu.classList.remove('show-hamburger');
+      content.classList.remove('blur');
+    });
+  })
+}
--- site/scripts/header.js
+++ site/scripts/header.js
@@ -1,7 +1,9 @@
 import { dynamicHighlight } from "./dynamicHighlight.js";
+import { hamburger } from "./hamburger.js";
 import { activeHeaderItem } from "./activeHeaderItem.js";
 import { headerScrollHide } from "./headerScrollHide.js";
 
 dynamicHighlight();
+hamburger();
 // activeHeaderItem();
 // headerScrollHide();
