ab01e493c4aca3def2a59d82d0db837242019363
Silviu Iordache <silviu.iordache92@gmail.com>
2021-09-09 22:55:58 +0300

    Fix bug with mobile header script

--- site/scripts/dynamicHighlight.js
+++ site/scripts/dynamicHighlight.js
@@ -14,20 +14,22 @@ export function dynamicHighlight() {
 
   // on scroll, determine which section is currently in view
   window.addEventListener('scroll', () => {
-    let current = '';
-    current = secDetails.find( (section) => {
+    let sectionInView = '';
+    sectionInView = secDetails.find( (section) => {
       return section.offsetTop > (pageYOffset - section.height / 3 )
     });
     
-    // highlight in the navitems
-    [...navItems].forEach( navItem => {
-      // remove active class from all
-      navItem.classList.remove('active');
-
-      // add active class to the element corresponding to the section in view
-      if(current.name.length > 0 && navItem.href.includes(current.name)) {
-        navItem.classList.add('active')
-      }
-    })
+    if (sectionInView) {
+      // highlight in the navitems
+      [...navItems].forEach( navItem => {
+        // remove active class from all
+        navItem.classList.remove('active');
+  
+        // add active class to the element corresponding to the section in view
+        if(sectionInView.name.length > 0 && navItem.href.includes(sectionInView.name)) {
+          navItem.classList.add('active')
+        }
+      });
+    }
   });
 }
