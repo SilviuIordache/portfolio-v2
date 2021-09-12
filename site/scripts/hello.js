e8ca4b1760115859cea9ebe7215b898704cc8805
Silviu Iordache <silviu.iordache92@gmail.com>
2021-09-12 18:10:13 +0300

    Add hello

---
+++ site/scripts/hello.js
@@ -0,0 +1,3 @@
+export function hello() {
+  console.log('I see you 😎')
+}
--- site/scripts/scripts.js
+++ site/scripts/scripts.js
@@ -3,9 +3,8 @@ import { hamburger } from "./hamburger.js";
 import { customCursor } from './customCursor.js'
 import { activeHeaderItem } from "./activeHeaderItem.js";
 import { headerScrollHide } from "./headerScrollHide.js";
+import { hello } from "./hello.js";
 
 dynamicHighlight();
 hamburger();
-// customCursor();
-// activeHeaderItem();
-// headerScrollHide();
+hello();
